702b6d6b62c18c6f8697c3512a0e095cfe398771
Anna Komarovska <ankakom3@gmail.com>
2019-04-17 10:41:45 -0400

    Test

--- SchoolBus/org.group8.project/src/main/java/org/group8/schoolbus/org/group8/project/ExampleOfTableViewTest.java
+++ SchoolBus/org.group8.project/src/main/java/org/group8/schoolbus/org/group8/project/ExampleOfTableViewTest.java
@@ -1,12 +1,18 @@
 package org.group8.schoolbus.org.group8.project;
 
 import static org.junit.Assert.*;
+
+import org.json.JSONArray;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TableView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseButton;
 import javafx.stage.Stage;
@@ -45,6 +51,22 @@ public class ExampleOfTableViewTest extends ApplicationTest{
 	  @Test
 	  public void testEnglishInput () {
 	    clickOn("#filterField");
-	    write("ytr");
+	    write("hajerkfhkjhflewjfklwejiwejoirjweoijewoifj");
+	  }
+	  
+	  @Test
+	  public void testUrlData()
+	  {		
+		  String dataUrl = new String();
+		  dataUrl = "https://data.cityofnewyork.us/resource/fbkk-fqs7.json";
+		  JSONArray testUrl = null;
+		
+		  try {
+			testUrl = new JSONArray(busData.getHTML(dataUrl));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		};
+		  assertEquals("https://data.cityofnewyork.us/resource/fbkk-fqs7.json", testUrl);
 	  }
 }
