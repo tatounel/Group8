fd608e9bfaccbfa3fc063a2108c1b2ffa29e5d85
Tania Nelzy <tania@Tanias-MacBook-Air.local>
2019-04-17 09:00:48 -0400

    Testing

---
+++ SchoolBus/org.group8.project/src/main/java/org/group8/schoolbus/org/group8/project/ExampleOfTableViewTest.java
@@ -0,0 +1,50 @@
+package org.group8.schoolbus.org.group8.project;
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseButton;
+import javafx.stage.Stage;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.testfx.api.FxToolkit;
+import org.testfx.framework.junit.ApplicationTest;
+
+
+public class ExampleOfTableViewTest extends ApplicationTest{
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+	  public void start (Stage stage) throws Exception {
+	    Parent mainNode = FXMLLoader.load(getClass().getResource("/ExampleOfTableView.fxml"));
+	    stage.setScene(new Scene(mainNode));
+	    stage.show();
+	    stage.toFront();
+	  }
+
+	  @Before
+	  public void setUp () throws Exception {
+	  }
+
+	  @After
+	  public void tearDown () throws Exception {
+	    FxToolkit.hideStage();
+	    release(new KeyCode[]{});
+	    release(new MouseButton[]{});
+	  }
+
+	  @Test
+	  public void testEnglishInput () {
+	    clickOn("#filterField");
+	    write("ytr");
+	  }
+}
